d34b6f73938221f5bbea8cd921bee634d76e3666
nazmulfci <nazmulfci@yahoo.com>
2024-02-29 16:34:43 +0600

    navbar

---
+++ src/components/Footer.vue
@@ -0,0 +1,59 @@
+<script setup>
+</script>
+<template>
+    <div class="bg-current h-auto py-10">
+        <div class="container mx-auto">
+            <div class="footer-main-box grid gap-x-8 gap-y-4 md:grid-cols-3 sm:2">
+                <div class="bg-gray-200 lg:mt-8">
+                    <h2 class="text-2xl text-center font-semibold mt-3 text-orange-500">Address</h2>
+                    <ul class="py-5 px-3 text-black text-lg">
+                        <li><a href="#"> <span class="text-orange-500"><i class="fa-sharp fa-solid fa-location-dot"></i></span> House#25,Main-Road,Block#G,Banasree Dhaka -1219</a></li>
+                        <li><a href="#"> <span class="text-orange-500"><i class="fa-solid fa-phone"></i></span> +8801812454358</a></li>
+                        <li><a href="#"> <span class="text-orange-500"><i class="fa-solid fa-envelope"></i></span> MAIL:info@doofazit.com</a></li>
+                        <li><a href="#"> <span class="text-orange-500"><i class="fa-solid fa-globe"></i></span> WEB :www.doofazit.com</a></li>
+                    </ul>
+                </div>
+
+                <div class="bg-gray-200 lg:mt-8 ">
+                    <h2 class="text-2xl text-center font-semibold mt-3 text-orange-500">Software Development</h2>
+                    <ul class="py-5 px-3 text-black text-lg">
+                        <li><a href="#"> <span class="text-orange-500"><i class="fa-solid fa-paper-plane"></i></span> Online POS (সব ধরনের ব্যাবসার জন্য)</a></li>
+                        <li><a href="#"> <span class="text-orange-500"><i class="fa-solid fa-paper-plane"></i></span> Business ERP(Completed Solutions)</a></li>
+                        <li><a href="#"> <span class="text-orange-500"><i class="fa-solid fa-paper-plane"></i></span> Trasnport ERP</a></li>
+                        <li><a href="#"> <span class="text-orange-500"><i class="fa-solid fa-paper-plane"></i></span> Human Resource-HRM</a></li>
+                        <li><a href="#"> <span class="text-orange-500"><i class="fa-solid fa-paper-plane"></i></span> সকল দোকান দারদের জন্য(POS)</a></li>
+                        <li><a href="#"> <span class="text-orange-500"><i class="fa-solid fa-paper-plane"></i></span> Hospital Management</a></li>
+                        <li><a href="#"> <span class="text-orange-500"><i class="fa-solid fa-paper-plane"></i></span> Tours & Travels</a></li>
+                        <li><a href="#"> <span class="text-orange-500"><i class="fa-solid fa-paper-plane"></i></span> খামারিদের সফটওয়্যার</a></li>
+                        <li><a href="#"> <span class="text-orange-500"><i class="fa-solid fa-paper-plane"></i></span> সমবায় সমিতি সফটওয়্যার</a></li>
+                    </ul>
+                </div>
+                
+                <div class="">
+                    <img class="h-56 lg:mt-10 w-72 ml-14" src="../assets/logo/google.png" alt="">
+                </div>
+            </div>
+            <!-- ===============================Copy-Right-Area============================== -->
+            <footer class="rounded-lg shadow m-4 dark:bg-gray-800">
+                <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+                <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024<a href="#" class="hover:underline"> DoofazIT LTD</a>. All Rights Reserved.</span>
+                    <h2 class="text-orange-500"><img src="../assets/logo/logo.png" alt="Doofazit-logo"></h2>
+                </div>
+            </footer>
+            <!-- ===============================Copy-Right-Area End============================== -->
+        </div>
+    </div>
+</template>
+<style>
+.footer-main-box h2{
+    background-image: linear-gradient(-225deg, #37a000 0%, #29B841 29%, #1148d7 67%, #fff800 100%);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+}
+@media all and (min-width:320px) and (max-width:780px){
+.footer-main-box{
+    width: 90%;
+    margin-left: 5%;
+}
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,162 @@
+<script setup>
+import 'flowbite';
+</script>
+
+<template>
+<nav class="bg-gray-300 lg:fixed w-full z-40 top-0 start-0 transition-all">
+  <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+    <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
+        <img src="../assets/logo/logo.png" class="h-8" alt="Doofazit-Logo" />
+    </a>
+    <button data-collapse-toggle="navbar-dropdown" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-dropdown" aria-expanded="false">
+        <span class="sr-only">Open main menu</span>
+        <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+        </svg>
+    </button>
+    <div class="hidden w-full md:block md:w-auto" id="navbar-dropdown">
+      <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-gray-300 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+        <li>
+            <button id="dropdownNavbarLink" data-dropdown-toggle="dropdownNavbar" class="flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent">Company <svg class="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+              <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/></svg>
+          </button>
+            <!-- Dropdown menu -->
+            <div id="dropdownNavbar" class="z-10 hidden font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600">
+                <ul class="py-2 text-sm text-gray-700 dark:text-gray-400 " aria-labelledby="dropdownLargeButton">
+                  <li>
+                    <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
+                  </li>
+                  <li>
+                    <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
+                  </li>
+                  <li>
+                    <a href="#" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
+                  </li>
+                </ul>
+            </div>
+        </li>
+        <li>
+                    <button id="mega-menu-dropdown-button" data-dropdown-toggle="mega-menu-dropdown" class="flex items-center justify-between w-full py-2 px-3 font-medium text-gray-900 border-b border-gray-100 md:w-auto hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">Software<svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                      <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+                    </svg>
+                    </button>
+                    <div id="mega-menu-dropdown" class="absolute z-10 grid hidden w-auto grid-cols-2 text-sm bg-white border border-gray-100 rounded-lg shadow-md dark:border-gray-700 md:grid-cols-3 dark:bg-gray-700">
+                        <div class="p-4 pb-0 text-gray-900 md:pb-4 dark:text-white">
+                            <ul class="space-y-4" aria-labelledby="mega-menu-dropdown-button">
+                                <li>
+                                    <a href="#" class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
+                                        About Us
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
+                                        Library
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
+                                        Resources
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
+                                        Pro Version
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="p-4 pb-0 text-gray-900 md:pb-4 dark:text-white">
+                            <ul class="space-y-4">
+                                <li>
+                                    <a href="#" class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
+                                        Blog
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
+                                        Newsletter
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
+                                        Playground
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
+                                        License
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                        <div class="p-4">
+                            <ul class="space-y-4">
+                                <li>
+                                    <a href="#" class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
+                                        Contact Us
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
+                                        Support Center
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="text-gray-500 dark:text-gray-400 hover:text-blue-600 dark:hover:text-blue-500">
+                                        Terms
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </li>
+        <li>
+          <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Website</a>
+        </li>
+        <li>
+          <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Digital Marketing</a>
+        </li>
+        <li>
+          <a href="#" class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Payment</a>
+        </li>
+      </ul>
+    </div>
+
+    <div class="hot-line">
+      <a href="#" class="text-xl text-red-600"><span class=" "><i class="fa-solid fa-phone-volume"></i></span> Hot-Line</a>
+      <h2 class="text-xl text-green-500 ">Call: (+880)01884-724029</h2>
+    </div>
+
+    <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 object-none">Get started</button>
+  </div>
+  
+</nav>
+
+</template>
+<style>
+  .hot-line span i{
+     color: red;
+   }
+   .hot-line span i:hover{
+    zoom: 1.2;
+   }
+   .text-green-500 {
+    margin-left: 25px 
+    }
+    #mega-menu-dropdown{
+      border-top: 4px solid #2eb71e;
+    }
+@media all and (min-width:320px) and (max-width:780px){
+  .hot-line{
+    display:none;
+  }
+  .text-white {
+    display: none;
+}
+#mega-menu-dropdown{
+
+    width: 95%;
+    border-top: 4px solid #2eb71e;
+  }
+}
+</style>
