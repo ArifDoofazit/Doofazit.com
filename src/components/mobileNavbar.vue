490ee680d4a5194fdff14545b7cf0ec4fe7c8fba
nazmulfci <nazmulfci@yahoo.com>
2024-03-14 11:27:47 +0600

    file update

--- src/components/mobileNavbar.vue
+++ src/components/mobileNavbar.vue
@@ -1,36 +1,12 @@
 <script setup>
 import 'flowbite';
-const $targetEl = document.getElementById('modalEl');
-
-// options with default values
-const options = {
-    placement: 'bottom-right',
-    backdrop: 'dynamic',
-    backdropClasses:' bg-Function fixed inset-0 z-40',
-    closable: true,
-    onHide: () => {
-        console.log('modal is hidden');
-    },
-    onShow: () => {
-        console.log('modal is shown');
-    },
-    onToggle: () => {
-        console.log('modal has been toggled');
-    },
-};
-
-// instance options object
-const instanceOptions = {
-  id: 'modalEl',
-  override: true
-};
-
 </script>
 
 <template>
 <nav class="md:flex lg:hidden sm:block bg-white z-50 fixed w-full p-2 top-0 h-auto">
   <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
 
+    
   <div class="block space-y-4 md:flex md:space-y-0 md:space-x-4 md:rtl:space-x-reverse">
     <button data-modal-target="top-left-modal" data-modal-toggle="top-left-modal" class="block w-full md:w-auto text-blue-500 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
       <span class="sr-only">Open main menu</span>
@@ -44,16 +20,16 @@ const instanceOptions = {
       <img src="../assets/logo/logo.png" class="w-64 -ml-5" alt="Doofazit-Logo"/>
     </a>
 <!-- =================================modal========================== -->
-<div id="top-left-modal" data-modal-placement="top-left" tabindex="-1" class="animate-left-to-right fixed top-0 left-0 right-0 z-50 hidden w-80 p-4 overflow-y-auto max-h-full">
-    <div class="relative w-full max-w-2xl max-h-full">
+<div id="top-left-modal" data-modal-placement="top-left" tabindex="-1" class="animate-left-to-right fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto max-h-full">
+    <div class="relative max-w-2xl max-h-full">
         <!-- Modal content -->
-        <div class="relative bg-white rounded-lg shadow">
+        <div class="content relative bg-white rounded-lg shadow dark:bg-gray-700">
             <!-- Modal header -->
-            <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+            <div class="bg-gray-50 flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
               <a href="#" class="logo-container animate-right-to-left">
                   <img src="../assets/logo/logo.png" class="w-auto" alt="Doofazit-Logo"/>
               </a>
-                <button type="button" class="text-gray-400 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:text-white" data-modal-hide="top-left-modal" style="border: 2px solid red;">
+                <button type="button" class="text-gray-400 hover:bg-gray-200 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:text-white" data-modal-hide="top-left-modal" style="border: 2px solid red;">
                     <svg class="w-3 h-3 text-red-600 font-bold" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                         <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
                     </svg>
@@ -62,13 +38,12 @@ const instanceOptions = {
             </div>
             
             <!-- Modal footer -->
-      <ul class="animate-left-to-right flex flex-col font-medium h-auto p-4 md:p-0 mt-4 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 dark:border-gray-700">
+      <ul class="animate-left-to-right flex flex-col font-medium h-auto p-4 md:p-0 mt-4 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-gray-300 dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
         <!-- ===================================Company================================ -->
-
-            <button type="button" class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group dark:text-white" aria-controls="dropdownNavbar" data-collapse-toggle="dropdownNavbar">
+            <button type="button" class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700" aria-controls="dropdownNavbar" data-collapse-toggle="dropdownNavbar">
                   <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap text-lg font-bold">Company</span>
                   <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                     <path class="text-blue-500" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
                   </svg>
             </button>
               <ul id="dropdownNavbar" class="hidden py-2 space-y-2">
@@ -100,7 +75,7 @@ const instanceOptions = {
             <button type="button" class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700" aria-controls="dropdownNavbar-sf" data-collapse-toggle="dropdownNavbar-sf">
                   <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap text-lg font-bold">Software</span>
                   <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                     <path class="text-blue-500" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
                   </svg>
             </button>
               <ul id="dropdownNavbar-sf" class="hidden py-2 space-y-2">
@@ -141,10 +116,11 @@ const instanceOptions = {
 <!-- ===================================softower End================================ -->
 
 <!-- ===================================Website End================================ -->
+          
             <button type="button" class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700" aria-controls="dropdownNavbarwe" data-collapse-toggle="dropdownNavbarwe">
                   <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap text-lg font-bold">Website</span>
                   <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                     <path class="text-blue-500" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
                   </svg>
             </button>
               <ul id="dropdownNavbarwe" class="hidden py-2 space-y-2">
@@ -166,10 +142,11 @@ const instanceOptions = {
 
 <!-- =================================Digital Marketing================================== -->
 
+          
             <button type="button" class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700" aria-controls="dropdownNavbar-dg" data-collapse-toggle="dropdownNavbar-dg">
                   <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap text-lg font-bold">Digital Marketing</span>
                   <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                     <path class="text-blue-500" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
                   </svg>
             </button>
               <ul id="dropdownNavbar-dg" class="hidden py-2 space-y-2">
@@ -193,10 +170,11 @@ const instanceOptions = {
 
 <!-- ========================SMS Marketing============================ -->
 
+          
             <button type="button" class="flex items-center w-full p-2 text-base text-gray-900 transition duration-75 rounded-lg group hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700" aria-controls="dropdownNavbar-sm" data-collapse-toggle="dropdownNavbar-sm">
                   <span class="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap text-lg font-bold">SMS Marketing</span>
                   <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
-                     <path class="text-blue-500" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
                   </svg>
             </button>
               <ul id="dropdownNavbar-sm" class="hidden py-2 space-y-2">
@@ -229,8 +207,8 @@ const instanceOptions = {
 </template>
 
 <style scoped>
-#top-left-modal{
-  border: 4px solid #000000;
+.content{
+  border: 5px solid black;
   background: #4097c7;
 }
 
@@ -302,6 +280,7 @@ const instanceOptions = {
     display: none;
 }
 #mega-menu-dropdown{
+
     width: 95%;
     border-top: 4px solid #2eb71e;
   }
